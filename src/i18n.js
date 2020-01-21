278ae9b37ef5f2ee69dc7d2375ab5d9c191c9ea8
aplchian <alex.boquist@sovereign.co>
2020-01-20 21:06:07 -0500

    feat: rename resource

--- src/i18n.js
+++ src/i18n.js
@@ -5,7 +5,7 @@ import getTranslations from '../src/translations/index'
 // (tip move them in a JSON file and import them)
 const translations = getTranslations()
 
-const x = {
+const resources = {
     en: {
         translation: translations.en,
     },
@@ -16,7 +16,7 @@ const x = {
 
 i18n.use(initReactI18next) // passes i18n down to react-i18next
     .init({
-        resources: x,
+        resources,
         lng: 'en',
         interpolation: {
             escapeValue: false, // react already safes from xss
