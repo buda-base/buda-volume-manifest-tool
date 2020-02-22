7dc2638d3997bfb4657a120a603ccaf19ed8e15e
eroux <elie.roux@telecom-bretagne.eu>
2020-02-22 17:58:32 +0100

    actually implement comparator

--- src/utils/pagination-comparators.js
+++ src/utils/pagination-comparators.js
@@ -1,7 +1,12 @@
 
 
 function getComparator(sectionList) {
-	return function(pagination1, section1, pagination2, section2) {
-		return 1;
+	return function(a_pagination, a_section, b_pagination, b_section) {
+		/* TODO: 
+		 * first, if a_section and b_section are not the same, compare their index in the sectionList
+		 * if they're the same, get the paginationtype (from pagination-prediction.js) of the section 
+		 * and put it in the "ptype" variable. Then:
+		 */
+		return ptype.compare(a_pagination, b_pagination);
 	}
 }
