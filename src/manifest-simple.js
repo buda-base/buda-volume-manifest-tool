2afe8e22e9c44256e2e7494d6a2a5b8555ba0e7a
eroux <elie.roux@telecom-bretagne.eu>
2020-03-03 14:51:27 +0100

    better data template, improve tag definition

--- src/manifest-simple.js
+++ src/manifest-simple.js
@@ -1,96 +1,93 @@
 import uuidv4 from 'uuid/v4'
 
 const data = {
-    'for-volume': 'bdr:V4CZ5369_I1KG9128',
-    'spec-version': '0.1.0',
-    rev: '860f508e-608e-484d-9fc3-62392fab0b12',
-    attribution: 'data contributed by rKTs',
-    'default-string-lang': 'en',
-    note: 'not sure about the order of page 2',
-    'changes-history': [
+    "for-volume": "bdr:I1KG9128",
+    "spec-version": "0.1.0",
+    "rev": "860f508e-608e-484d-9fc3-62392fab0b12",
+    "attribution": [{"@value": "data contributed by rKTs", "@language": "en"}],
+    "note": [{"@value":"not sure about the order of page 2"}],
+    "viewing-direction": "left-to-right",
+    "changes": [
         {
-            timestamp: 1568463794,
-            user: 'bdr:U0002',
-            log: 'initial version',
-        },
+            "time": "2020-02-03T11:35:30.00Z",
+            "user": "bdr:U0002",
+            "message": {"@value": "initial version", "@language": "en"}
+        }
     ],
-    pagination: {
-        folios1: {
-            type: 'potifolios',
-            name: 'original pagination',
+    "pagination": [
+        {   
+            "id": "pgfolios",
+            "type": "folios",
+            "note": [{"@value": "from the original blockprint", "@language": "en"}]
         },
-    },
-    'pagination-default': 'folios1',
-    'view-default': 'view1',
-    view: {
-        view1: {
-            imagelist: [
-                {
-                    id: 1,
-                    subtype: 'cover',
-                    filename: 'I1KG91280001.tif',
-                    chips: [{ id: 2, text: 'Cover Page' }],
-                },
-                {
-                    id: 2,
-                    subtype: 'blank',
-                    filename: 'I1KG91280002.tif',
-                    pagination: '1a',
-                    chips: [{ id: 1, text: 'Title Page' }],
-                },
-                {
-                    id: 3,
-                    subtype: 'title',
-                    thumbnail: true,
-                    filename: 'I1KG91280003.jpg',
-                    pagination: '1b',
-                },
-                {
-                    id: 4,
-                    filename: 'I1KG91280005.jpg',
-                    pagination: "1'a",
-                    rotation: 180,
-                    'rotation-reason': 'scan',
-                    note: [
-                        'rotating 180° because of the scan being upside down',
-                    ],
-                },
-                {
-                    id: 5,
-                    type: 'missing',
-                    subtype: 'from-scan',
-                    pagination: "1'b",
-                },
-                {
-                    id: 6,
-                    type: 'duplicate',
-                    filename: 'I1KG91280006.jpg',
-                    subtype: 'from-scan',
-                    display: false,
-                    'of-file': 'I1KG9128004.tif',
-                },
-            ],
+        {   
+            "id": "bates",
+            "type": "simple",
+            "note": [{"@value": "bates numbers added on the xerox reprint", "@language": "en"}]
+        }
+    ],
+    "sections": [
+        {
+            "id": "intro",
+            "name": {"@value": "intro", "@language": "en"}
         },
-    },
-    volumeData: {
-        volume: 'bdr:V22084_I0888',
-        defaultLanguage: 'en',
-        volumeLanguage: 'tibetan',
-        showCheckedImages: true,
-        showHiddenImages: true,
-        viewingDirection: 'left-to-right',
-        inputOne: {
-            paginationType: 'folios',
-            inputForWholeMargin: true,
-            sectionInputs: [
-                { value: 'Section 1a', language: 'bo', id: uuidv4() },
-                { value: 'Section 2a', language: 'bo', id: uuidv4() },
-            ],
-            indicationOdd: '{volname}-{sectionname}-{pagenum:bo}',
-            indicationEven: '{volname}',
+        {
+            "id": "s1",
+            "name": {"@value": "section1", "@language": "bo-x-ewts"}
         },
-        comments: '',
+        {
+            "id": "s2",
+            "name": {"@value": "section2", "@language": "bo-x-ewts"}
+        }
+    ],
+    "default-view": "view1",
+    "view": {
+        "view1": {
+            "imagelist": [
+                {
+                    "filename": "I1KG9128001.tif",
+                    "pagination": { "pgfolios": {"value": "1a", "section": "intro"} },
+                    "note": [
+                        {"@value": "this is an intro page, some editions would have that before the scans", "@language": "en"}
+                    ]
+                },
+                {
+                    "filename": "I1KG9128002.tif",
+                    "pagination": { "pgfolios": {"value": "1a", "section": "intro"} },
+                    "indication": { "@value": "kha", "@language": "bo-x-ewts" },
+                    "tags": ["T0021", "T0011"]
+                },
+                {
+                    "pagination": { "pgfolios": {"value": "1a"} },
+                    "tags": ["T0020"]
+                },
+                {
+                    "filename": "I1KG9128003.tif",
+                    "pagination": { "pgfolios": {"value": "1b", "section": "intro"}, "bates": {"value": "2"} }
+                },
+                {
+                    "filename": "I1KG9128005.tif",
+                    "display": false,
+                    "of": "I1KG9128004.tif"
+                },
+                {
+                    "filename": "I1KG9128004.tif",
+                    "pagination": { "pgfolios": {"value": "1'a", "section": "intro"} },
+                    "rotation": 180
+                }
+            ]
+        }
     },
+    "appData": {
+        "bvmt": {
+            "metadata-for-bvmt-ver": "0.1.0",
+            "default-string-lang": "en",
+            "show-checked-images": true,
+            "show-hidden-images": true,
+            "margin-indication-odd": "{volname}-{sectionname}-{pagenum:bo}",
+            "margin-indication-even": ""
+        }
+    }
 }
 
 export default data
